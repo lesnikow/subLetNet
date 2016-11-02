92f4126bfa369e00e56e60155c87146d7ae6ea2b
lesnikow <lesnikow@users.noreply.github.com>
2016-11-02 11:59:30 -0400

    added bos10 to debug, work on getting tf.argmax to print in sess

--- subLetNet.py
+++ subLetNet.py
@@ -32,7 +32,7 @@ def readImages(imgDir) :
   row = 171
   col = 256
   images = np.zeros((len(os.listdir(imgDir)), row * col))
-  print(images.shape)
+  #print(images.shape)
   imageIds = []
   index = 0
   for imageName in os.listdir(imgDir) :
@@ -89,18 +89,20 @@ def readLabels(labelPath) :
     return priceBins
 
 # read in data
-imgDir = 'bos100/'
+imgDir = 'bos10/'
 labelPath = 'labels/bosPrices.csv'
-
 # images
 imageIds, images = readImages(imgDir)
+
 # ground truth (labels)
 priceBins = readLabels(labelPath)
+#print(priceBins)
 # find label (prince bin) for 
 # each data point in training dataset
 labels = []
 for id in imageIds :
     labels.append(priceBins[id])
+print('labels are %s' % labels)
 
 # img attr
 row = 171
@@ -111,10 +113,12 @@ numImages = len(imageIds)
 numBins = 10
 
 # ----------------------- START CNN --------------------------------
-sess = tf.InteractiveSession()
+#sess = tf.InteractiveSession()
 
 x = tf.placeholder(tf.float32, [None, imgLength])
 y_ = tf.placeholder(tf.int64, [None])
+
+prediction = tf.placeholder(tf.float32, [None])
 
 # first convolution layer
 W_conv1 = weight_variable([5, 5, 1, 32])
@@ -137,7 +141,7 @@ h_pool2 = max_pool_2x2(h_conv2)
 
 # pooling layer
 # this is how the input size is changed after two convolution layers
-# 	- for each max_pooling, input size shrinks to half (there are two max poolings)
+#   - for each max_pooling, input size shrinks to half (there are two max poolings)
 W_fc1 = weight_variable([int(math.ceil(row / 4) * math.ceil(col / 4)) * 64, 1024])
 b_fc1 = bias_variable([1024])
 
@@ -159,21 +163,35 @@ cross_entropy = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(y_
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv,1), y_)
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-sess.run(tf.initialize_all_variables())
-for i in range(numImages):
-	print(str(i) + '/' + str(numImages))
-	img = images[i, :]
-	label = labels[i]
-	labelVector = [0 for element in range(numBins)]
-	labelVector[label] = 1
-	if i%10 == 0:
-	    train_accuracy = accuracy.eval(feed_dict={
-	        x:[img], y_: [label], keep_prob: 1.0})
-	    print("step %d, training accuracy %g"%(i, train_accuracy))
-	print('label: ' + str(label) + ' & prediction: ' + str(tf.argmax(y_conv, 1)))
-	train_step.run(feed_dict={x: [img], y_: [label], keep_prob: 0.5})
 
-print("test accuracy %g"%accuracy.eval(feed_dict={
-    x: images, y_: labels, keep_prob: 1.0}))
+with tf.Session() as sess:
+    sess.run(tf.initialize_all_variables())
+
+    for i in range(numImages):
+        print(str(i) + '/' + str(numImages))
+        img = images[i, :]
+        label = labels[i]
+        labelVector = [0 for element in range(numBins)]
+        labelVector[label] = 1
+        if i%10 == 0:
+            train_accuracy = accuracy.eval(feed_dict={
+                x:[img], y_: [label], keep_prob: 1.0})
+            print("step %d, training accuracy %g"%(i, train_accuracy))
+        print('label: ' + str(label) + '\n')
+
+        prediction  = tf.argmax(y_conv, 1)
+
+        #print(sess.run(prediction))
+        
+        #sess2 = tf.Session()
+        #print(sess2.run(prediction))
+        
+        #print('prediction: %s' % prediction)
+     
+        #print('label: ' + str(label) + ' & prediction: ' + str(tf.argmax(y_conv, 1)))
+        train_step.run(feed_dict={x: [img], y_: [label], keep_prob: 0.5})
+
+    print("test accuracy %g"%accuracy.eval(feed_dict={
+        x: images, y_: labels, keep_prob: 1.0}))
 
 # ----------------------- END --------------------------------
